e48b9d49e4a6ea7f01c620e20dce88cb749c71b1
EntityBlackHawk <luanf2003@gmail.com>
2024-03-08 19:51:37 -0300

    feat: Random number generation

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,15 @@
 #include <MqttConnection.h>
 #include <cstdlib>
 #include <thread>
+#include <sstream>
+#include <random>
 
-constexpr std::string topic = "teste";
-constexpr std::string message = "OK - 2";
+std::string topic = "test/temperature";
+int temperature = 25;
+
+std::random_device rd;
+std::mt19937 mt(rd());
+std::uniform_int_distribution dist(-3, 3);
 
 /*
  *
@@ -16,6 +22,20 @@ constexpr std::string message = "OK - 2";
  * */
 
 int main() {
+
+    std::stringstream ss;
+
+   ss << " _____               _   ___   _                              ___\n";
+   ss << "|_   _|             | | |  _| | |                            |_  |\n";
+   ss << "  | |    _ __     __| | | |   | |__    _   _    __ _    ___    | |\n";
+   ss << "  | |   | '_ \\   / _` | | |   | '_ \\  | | | |  / _` |  / _ \\   | |\n";
+   ss << " _| |_  | | | | | (_| | | |   | | | | | |_| | | (_| | |  __/   | |\n";
+   ss << "|_____| |_| |_|  \\__,_| | |_  |_| |_|  \\__,_|  \\__, |  \\___|  _| |\n";
+   ss << "                        |___|                   __/ |        |___|\n";
+   ss << "                                               |___/";
+
+
+    std::cout << ss.str() << std::endl;
 
     TlsConfig tlsc;
     tlsc.key_path = std::getenv("MQTT_KEY_PATH");
@@ -36,10 +56,12 @@ int main() {
 
    auto mc = MqttConnection(host, std::stoi(port), tlsc);
 
-    for(int i = 0; i < 5; i++) {
-        std::cout << "Send message" << std::endl;
-        mc.publish(const_cast<std::string &>(topic), const_cast<std::string &>(message));
-        std::this_thread::sleep_for(std::chrono::seconds(5));
+    while(true) {
+        std::stringstream ms;
+        ms << temperature + dist(mt);
+        mc.publish(topic, (std::string &) std::move(ms.str()));
+        std::cout << topic<< " " << ms.str() << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 
 
